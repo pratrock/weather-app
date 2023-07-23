87fc0cc97bb7e32f10c4a4dfc0c55558a93839d1
Prateek Parmar <30598262+pratrock@users.noreply.github.com>
2023-07-23 17:22:04 -0500

    Added search functionality and user location tracking

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const api = {
+    key: "44df51f40318c861bac7f73939f8787b",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }, [lat, long]);
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const getLocation = async () => {
+    await fetch(
+      `${api.base}weather/?lat=${lat}&lon=${long}&units=metric&APPID=${api.key}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        setQuery("");
+        console.log(result);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="search.."
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyUp={search}
+      />
+      <button onClick={getLocation}>Add you location automatically</button>
     </div>
   );
 }
