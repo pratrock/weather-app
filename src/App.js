f77f60d21422c717b4716511f42326f5ab903ded
Prateek Parmar <30598262+pratrock@users.noreply.github.com>
2023-08-02 18:36:10 -0400

    added UI code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-
+import "./App.css";
 function App() {
   const api = {
     key: "44df51f40318c861bac7f73939f8787b",
@@ -17,6 +17,39 @@ function App() {
       console.log("Longitude is :", position.coords.longitude);
     });
   }, [lat, long]);
+
+  const buildDate = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}${date}${month}${year}`;
+  };
   const search = async (e) => {
     if (e.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&appid=${api.key}&units=metric`)
@@ -42,15 +75,44 @@ function App() {
   };
 
   return (
-    <div>
-      <input
-        type="search.."
-        placeholder="Search..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyUp={search}
-      />
-      <button onClick={getLocation}>Add you location automatically</button>
+    <div
+      className={
+        typeof weather.main !== "undefined"
+          ? weather.main.temp > 16
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-container">
+          <input
+            type="search.."
+            placeholder="Search..."
+            value={query}
+            className="search"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyUp={search}
+          />
+          <button onClick={getLocation}>Add you location automatically</button>
+        </div>
+
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className="date">{buildDate(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
     </div>
   );
 }
